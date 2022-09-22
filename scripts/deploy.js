5c0da6a3d9ec06081b7401848fe74c5618f32d82
kaushikkishore <kaushik@vauld.com>
2022-09-22 20:35:32 +0530

    accessing the contract functions

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -16,12 +16,22 @@ async function main() {
 
   console.log(`Deployed contract to ${simpleStorage.address}`);
 
-  console.log(network.config);
+  // console.log(network.config);
   // No need to call this when you are working on your local network
   if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
     await simpleStorage.deployTransaction.wait(7);
     await verify(simpleStorage.address, []);
   }
+
+  // Access the contract function
+  const value = await simpleStorage.getValue();
+  console.log(`Current value = ${value}`);
+
+  const transactionResponse = await simpleStorage.setValue(1203);
+  await transactionResponse.wait(1);
+
+  const updatedValue = await simpleStorage.getValue();
+  console.log(`Updated Value = ${updatedValue}`);
 }
 
 async function verify(contractAddress, args) {

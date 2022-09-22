18e6e0967b05cc637f4cf0ab39636432e8fef3a8
kaushikkishore <kaushik@vauld.com>
2022-09-22 20:25:33 +0530

    updated verify

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 const RPC_URL = process.env.GOERLI_RPC_URL;
 const ACCOUNT = process.env.GOERLI_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 // You can find the chainId using - https://chainlist.org/
 module.exports = {
@@ -15,5 +18,8 @@ module.exports = {
       accounts: [ACCOUNT],
       chainId: 5,
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
 async function main() {
   const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
   console.log(`Deploying the contract`);
@@ -15,6 +15,32 @@ async function main() {
   // and API_Key which can get from metamask
 
   console.log(`Deployed contract to ${simpleStorage.address}`);
+
+  console.log(network.config);
+  // No need to call this when you are working on your local network
+  if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(7);
+    await verify(simpleStorage.address, []);
+  }
+}
+
+async function verify(contractAddress, args) {
+  console.log(`Verifying the contract ${contractAddress}`);
+  // look for the yarn hardhat verify --help
+
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log(`Contract already verified`);
+    } else {
+      console.log(`We ran to an error ${e.message}`);
+      console.log(e);
+    }
+  }
 }
 
 main()
